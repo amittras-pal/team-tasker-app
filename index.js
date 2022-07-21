8e8541b13692c85bce2ab85d939d7d02ca8a2700
amittras-pal <pal.amittras@gmail.com>
2022-07-21 09:56:21 +0530

    Cleanup

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ require("colors");
 
 const PORT = process.env.PORT;
 const dbConnection = require("./config/database.config");
-const { sendEmail } = require("./utils/mailer");
-const { httpStatus } = require("./constants/http.constants");
 dbConnection();
 
 const app = express();
@@ -15,29 +13,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.post("/send-email", async (req, res) => {
-  try {
-    const mail = await sendEmail({
-      to: ["alchemist6174@protonmail.com", "pal.amittras@gmail.com"],
-      subject: "Sending a mail, testing if it is still working.",
-      template: "verifyRegistration",
-      context: {
-        name: "Amittras Pal",
-        code: "123456",
-      },
-    });
-    return res.json({
-      message: "Email sent successfully.",
-      response: mail,
-    });
-  } catch (error) {
-    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      message: "Something went wrong",
-      error,
-    });
-  }
-});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
