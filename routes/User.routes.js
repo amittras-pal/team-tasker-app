8a6071c765c10488895e486dcac5c4124323481b
Lokesh Tiwari <lokesh0412@gmail.com>
2022-07-23 08:57:19 +0530

    corrected file name and removed folder-info

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { signUp, verifyToken } = require("../controllers/User.controller");
+const { signUp, verifyToken } = require("../controllers/user.controller");
 
 router.post("/signup", signUp);
 router.get("/verify-token/:userId/:token", verifyToken);
